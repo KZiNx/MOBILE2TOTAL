2cd7c1796f4cdced0f426396bdd7239077c44260
Kevin_Cristhian <subzeromaster007@hotmail.com>
2023-10-31 19:41:23 -0300

    tentativa

--- mobile2/App.js
+++ mobile2/App.js
@@ -4,24 +4,22 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from './src/HomeScreen';
-import EditTela from './src/EditTela';
+import ListScreen from './src/ListScreen';
 
+const Stack = createNativeStackNavigator();
 
-const Stack = createStackNavigator();
-
-function App() {
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="EditList" component={EditList} />
-  
+        <Stack.Screen name="Adicionar Lista" component={ListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
+
 }
 
-export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
