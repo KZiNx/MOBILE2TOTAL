7beabc8fa7c31eb32f82ab380ace1c99c7e8bbe3
Kevin_Cristhian <subzeromaster007@hotmail.com>
2023-11-07 19:27:38 -0300

    a

--- mobile2/App.js
+++ mobile2/App.js
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen  name="Home" component={HomeScreen} />
         <Stack.Screen name="Adicionar Lista" component={ListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -23,7 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- mobile2/src/HomeScreen/HomeScreen.js
+++ mobile2/src/HomeScreen/HomeScreen.js
@@ -1,46 +1,110 @@
-import { useState } from "react";
-import { Button, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Button, Text, View, FlatList, TouchableOpacity, TextInput } from "react-native";
 
 function HomeScreen({ navigation }) {
+  const [quantidadeLista, setQuantidadeLista] = useState(0);
+  const [listas, setListas] = useState([]);
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [editedName, setEditedName] = useState("");
 
-  let [quantidadeLista, setQuantidadeLista] = useState("0")
-
-    const addlist = () => {
-      if (quantidadeLista <= 10) {
-        setQuantidadeLista(quantidadeLista++)
-      } else {
-        alert("Você não pode mais adicionar listas")
-      }
+  const addList = () => {
+    if (quantidadeLista < 10) {
+      const newListas = [...listas, `Lista ${quantidadeLista + 1}`];
+      setListas(newListas);
+      setQuantidadeLista(quantidadeLista + 1);
+    } else {
+      alert("Você não pode mais adicionar listas");
     }
+  }
 
-    const deleteList = () => {
-      if (quantidadeLista >= 0) {
-        setQuantidadeLista(quantidadeLista--)
-      }
+  const deleteList = (index) => {
+    if (quantidadeLista > 0) {
+      const newListas = [...listas];
+      newListas.splice(index, 1);
+      setListas(newListas);
+      setQuantidadeLista(quantidadeLista - 1);
     }
+  }
 
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Aqui estão as suas listas</Text>
+  const editListName = (index) => {
+    setEditingIndex(index);
+    setEditedName(listas[index]);
+  }
 
-        <Button
-                title="Adicionar Lista"
-                onPress={() => navigation.navigate('Adicionar Lista')}
-            />
+  const saveEditedListName = () => {
+    if (editingIndex !== null) {
+      const newListas = [...listas];
+      newListas[editingIndex] = editedName;
+      setListas(newListas);
+      setEditingIndex(null);
+    }
+  }
 
-            <Button
-                title="add list"
-                onPress={addlist}
-            />
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Aqui estão as suas listas</Text>
 
-            <Button
-                title="del list"
-                onPress={deleteList}
-            />
+      <Button
+        title="Adicionar Lista"
+        onPress={() => navigation.navigate('Adicionar Lista')}
+      />
 
-        <Text>Você tem {quantidadeLista}/10</Text>
-      </View>
-    );
-  } 
+      <Button
+        title="Adicionar Lista"
+        onPress={addList}
+      />
 
-  export default HomeScreen;
+      <Button
+        title="Remover Lista"
+        onPress={() => deleteList(editingIndex)}
+      />
+
+      {editingIndex !== null ? (
+        <View>
+          <TextInput
+            value={editedName}
+            onChangeText={(text) => setEditedName(text)}
+          />
+          <Button
+            title="Salvar"
+            onPress={saveEditedListName}
+          />
+        </View>
+      ) : null}
+
+      <Text>Você tem {quantidadeLista}/10</Text>
+
+      <FlatList
+        data={listas}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <View>
+            <TouchableOpacity onPress={() => navigation.navigate('Detalhes da Lista', { lista: item })}>
+              {editingIndex === index ? (
+                <TextInput
+                  value={editedName}
+                  onChangeText={(text) => setEditedName(text)}
+                />
+              ) : (
+                <Text>{item}</Text>
+              )}
+            </TouchableOpacity>
+            {editingIndex === index ? (
+              <Button
+                title="Salvar"
+                onPress={saveEditedListName}
+              />
+            ) : (
+              <Button
+                title="Editar Nome"
+                onPress={() => editListName(index)}
+              />
+            )}
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+export default HomeScreen;
