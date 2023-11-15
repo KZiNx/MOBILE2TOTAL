d5d1366abb0fbf3e182f40fda1ab3a03e4159efd
Kevin_Cristhian <subzeromaster007@hotmail.com>
2023-11-14 21:46:56 -0300

    quase

--- mobile2/HomeScreen/HomeScreen.js
+++ mobile2/HomeScreen/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Text, View, FlatList, TouchableOpacity, TextInput } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, TextInput, StyleSheet } from "react-native";
 
 function HomeScreen({ navigation }) {
   const [quantidadeLista, setQuantidadeLista] = useState(0);
@@ -9,13 +9,20 @@ function HomeScreen({ navigation }) {
 
   const addList = () => {
     if (quantidadeLista < 10) {
-      const newListas = [...listas, `Lista ${quantidadeLista + 1}`];
+      const newListas = [
+        ...listas,
+        {
+          name: `Lista ${quantidadeLista + 1}`,
+          createdAt: new Date(),
+          lastModified: new Date(),
+        },
+      ];
       setListas(newListas);
       setQuantidadeLista(quantidadeLista + 1);
     } else {
       alert("Você não pode mais adicionar listas");
     }
-  }
+  };
 
   const deleteList = (index) => {
     if (quantidadeLista > 0) {
@@ -24,74 +31,172 @@ function HomeScreen({ navigation }) {
       setListas(newListas);
       setQuantidadeLista(quantidadeLista - 1);
     }
-  }
+  };
 
   const editListName = (index) => {
     setEditingIndex(index);
-    setEditedName(listas[index]);
-  }
+    setEditedName(listas[index].name);
+  };
 
   const saveEditedListName = () => {
     if (editingIndex !== null) {
       const newListas = [...listas];
-      newListas[editingIndex] = editedName;
+      newListas[editingIndex] = {
+        ...newListas[editingIndex],
+        name: editedName,
+        lastModified: new Date(),
+      };
       setListas(newListas);
       setEditingIndex(null);
     }
-  }
+  };
 
   const navigateToItemList = (listName) => {
     navigation.navigate('ItemList', { listName });
-  }
+  };
+
+ 
+  const sortedLists = listas.sort(
+    (a, b) => b.lastModified.getTime() - a.lastModified.getTime()
+  );
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Aqui estão as suas listas</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Aqui estão as suas listas</Text>
 
-      <Button
-        title="Adicionar Lista"
-        onPress={addList}
-      />
+      <TouchableOpacity style={styles.button} onPress={addList}>
+        <Text style={styles.buttonText}>Adicionar Lista</Text>
+      </TouchableOpacity>
 
-      <Text>Você tem {quantidadeLista}/10</Text>
+      <Text style={styles.text}>Você tem {quantidadeLista}/10</Text>
 
       <FlatList
-        data={listas}
+        data={sortedLists}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item, index }) => (
-          <View>
-            <TouchableOpacity onPress={() => navigateToItemList(item)}>
+          <View style={styles.listItemContainer}>
+            <TouchableOpacity
+              onPress={() => navigateToItemList(item.name)}
+              style={styles.button2}
+            >
               {editingIndex === index ? (
                 <TextInput
+                  style={styles.input}
                   value={editedName}
                   onChangeText={(text) => setEditedName(text)}
                 />
               ) : (
-                <Text>{item}</Text>
+                <View style={styles.buttonContainer2}>
+                  <Text style={styles.text}>{item.name}</Text>
+                  <Text style={styles.timestampText}>
+                    Última modificação: {item.lastModified.toLocaleString()}
+                  </Text>
+                </View>
               )}
             </TouchableOpacity>
-            {editingIndex === index ? (
-              <Button
-                title="Salvar"
-                onPress={saveEditedListName}
-              />
-            ) : (
-              <>
-                <Button
-                  title="Editar Nome"
-                  onPress={() => editListName(index)}
-                />
-                <Button
-                  title="Remover Lista"
-                  onPress={() => deleteList(index)}
-                />
-              </>
-            )}
+            <View style={styles.buttonContainer}>
+              {editingIndex === index ? (
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={saveEditedListName}
+                >
+                  <Text style={styles.buttonText}>Salvar</Text>
+                </TouchableOpacity>
+              ) : (
+                <>
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => editListName(index)}
+                  >
+                    <Text style={styles.buttonText}>Editar Nome</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => deleteList(index)}
+                  >
+                    <Text style={styles.buttonText}>Remover Lista</Text>
+                  </TouchableOpacity>
+                </>
+              )}
+            </View>
           </View>
         )}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#3c3c3c',
+   
+  },
+  buttonText: {
+    color: '#ffffff',
+    alignItems: 'center',
+    fontSize: 16, 
+    
+  },
+  button: {
+    backgroundColor: '#3c3c3c',
+    borderColor: '#ffffff',
+    borderWidth: 2,
+    padding: 10,
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  button2: {
+    backgroundColor: '#3c3c3c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#ffffff',
+    fontSize: 16,
+    margin: 10,
+    alignItems: 'center',
+  },
+  input: {
+    color: '#ffffff',
+    borderColor: '#ffffff',
+    borderWidth: 1,
+    padding: 10,
+    margin: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+
+   },
+    listItemContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      marginBottom: 10,
+    },
+  
+    buttonContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    buttonContainer2: {
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  listItemContainer: {
+
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  timestampText: {
+    color: "#ffffff",
+    fontSize: 12,
+    fontStyle: "italic",
+  },
+});
 
 export default HomeScreen;
--- mobile2/ItemList/ItemList.js
+++ mobile2/ItemList/ItemList.js
@@ -1,87 +1,226 @@
-import React, { useState } from "react";
-import { Button, Text, View, FlatList, TouchableOpacity, TextInput } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-function ItemList({ route, navigation }) {
-  const [lista, setLista] = useState({ name: route.params.listName, items: [] });
+const ItemList = ({ route, navigation }) => {
+  const [lista, setLista] = useState({
+    name: route.params.listName,
+    items: [],
+  });
   const [editingIndex, setEditingIndex] = useState(null);
   const [editedName, setEditedName] = useState("");
 
+  useEffect(() => {
+    const loadList = async () => {
+      try {
+        const storedList = await AsyncStorage.getItem(`Lista-${lista.name}`);
+        if (storedList) {
+          setLista({
+            name: lista.name,
+            items: JSON.parse(storedList),
+          });
+        }
+      } catch (error) {
+        console.error("Erro ao carregar lista do AsyncStorage:", error);
+      }
+    };
+
+    loadList();
+  }, [lista.name]);
+
+  useEffect(() => {
+    const saveList = async () => {
+      try {
+        await AsyncStorage.setItem(
+          `Lista-${lista.name}`,
+          JSON.stringify(lista.items)
+        );
+      } catch (error) {
+        console.error("Erro ao salvar lista no AsyncStorage:", error);
+      }
+    };
+
+    if (lista.items.length) {
+      saveList();
+    }
+  }, [lista]);
+
   const addItem = () => {
     if (lista.items.length < 10) {
-      const newItem = `Item ${lista.items.length + 1}`;
-      lista.items.push(newItem);
-      setLista({ ...lista });
+      const newItem = {
+        nome: `Item ${lista.items.length + 1}`,
+        dataAlteracao: new Date(),
+      };
+      setLista((prevList) => ({
+        ...prevList,
+        items: [...prevList.items, newItem],
+      }));
     } else {
       alert("Você não pode mais adicionar itens a esta lista");
     }
-  }
+  };
 
   const deleteItem = (index) => {
     if (lista.items.length > 0) {
-      lista.items.splice(index, 1);
-      setLista({ ...lista });
+      const updatedItems = [...lista.items];
+      updatedItems.splice(index, 1);
+      setLista((prevList) => ({ ...prevList, items: updatedItems }));
     }
-  }
+  };
 
   const editItemName = (index) => {
     setEditingIndex(index);
-    setEditedName(lista.items[index]);
-  }
+    setEditedName(lista.items[index].nome);
+  };
 
   const saveEditedItemName = () => {
     if (editingIndex !== null) {
-      lista.items[editingIndex] = editedName;
-      setLista({ ...lista });
+      const updatedItems = [...lista.items];
+      updatedItems[editingIndex] = {
+        nome: editedName,
+        dataAlteracao: new Date(),
+      };
+      setLista((prevList) => ({ ...prevList, items: updatedItems }));
       setEditingIndex(null);
     }
-  }
+
+    Keyboard.dismiss();
+  };
+
+  const sortedItems = lista.items.sort(
+    (a, b) => b.dataAlteracao - a.dataAlteracao
+  );
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Itens da Lista: {lista.name}</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Itens da Lista: {lista.name}</Text>
 
-      <Button
-        title="Adicionar Item"
-        onPress={addItem}
-      />
+      <TouchableOpacity style={styles.button} onPress={addItem}>
+        <Text style={styles.buttonText}>Adicionar Item</Text>
+      </TouchableOpacity>
 
-      <FlatList
-        data={lista.items}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item, index }) => (
-          <View>
-            <TouchableOpacity>
-              {editingIndex === index ? (
-                <TextInput
-                  value={editedName}
-                  onChangeText={(text) => setEditedName(text)}
-                />
-              ) : (
-                <Text>{item}</Text>
-              )}
-            </TouchableOpacity>
-            {editingIndex === index ? (
-              <Button
-                title="Salvar"
-                onPress={saveEditedItemName}
-              />
-            ) : (
-              <>
-                <Button
-                  title="Editar Nome"
-                  onPress={() => editItemName(index)}
-                />
-                <Button
-                  title="Remover Item"
-                  onPress={() => deleteItem(index)}
-                />
-              </>
-            )}
-          </View>
-        )}
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.keyboardAvoidingContainer}
+      >
+        <FlatList
+          data={sortedItems}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item, index }) => (
+            <View style={styles.itemContainer}>
+              <TouchableOpacity>
+                {editingIndex === index ? (
+                  <TextInput
+                    style={styles.editInput}
+                    value={editedName}
+                    onChangeText={(text) => setEditedName(text)}
+                    editable={true}
+                  />
+                ) : (
+                  <Text style={styles.text}>{item.nome}</Text>
+                )}
+              </TouchableOpacity>
+              <View style={styles.buttonContainer}>
+                {editingIndex === index ? (
+                  <TouchableOpacity
+                    style={styles.button2}
+                    onPress={saveEditedItemName}
+                  >
+                    <Text style={styles.buttonText}>Salvar</Text>
+                  </TouchableOpacity>
+                ) : (
+                  <>
+                    <TouchableOpacity
+                      style={styles.button}
+                      onPress={() => editItemName(index)}
+                    >
+                      <Text style={styles.buttonText}>Editar Nome</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={styles.button}
+                      onPress={() => deleteItem(index)}
+                    >
+                      <Text style={styles.buttonText}>Remover Item</Text>
+                    </TouchableOpacity>
+                  </>
+                )}
+              </View>
+            </View>
+          )}
+        />
+      </KeyboardAvoidingView>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#3c3c3c",
+  },
+  buttonText: {
+    color: "#ffffff",
+    fontSize: 16,
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: "#3c3c3c",
+    borderColor: "#ffffff",
+    borderWidth: 2,
+    padding: 10,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button2: {
+    backgroundColor: "#3c3c3c",
+    borderColor: "#ffffff",
+    borderWidth: 2,
+    padding: 10,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom : 200,
+  },
+  text: {
+    color: "#ffffff",
+    fontSize: 16,
+    margin: 10,
+    alignItems: 'center',
+  },
+  itemContainer: {
+    marginBottom: 10,
+    alignItems: 'center',
+  },
+  editInput: {
+    height: 40,
+    borderColor: "white",
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    color: "white",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+  },
+  keyboardAvoidingContainer: {
+    flex: 1,
+   
+    width: "100%",
+    
+  },
+});
 
 export default ItemList;
